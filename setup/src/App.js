f29baa2953e0dbc069c3b900db076a035fdf6d53
parjana94 <b.parjana@gmail.com>
2023-03-15 18:16:54 +0400

    ss

--- setup/src/App.js
+++ setup/src/App.js
@@ -6,9 +6,16 @@ import Values from 'values.js'
 function App() {
   const [color, setColor] = useState('')
   const [error, setError] = useState(false)
-  const [list, setList] = useState([])
+  const [list, setList] = useState(new Values('#f15025').all(10))
   const submitHendler = (e) => {
     e.preventDefault()
+    try {
+      const colors = new Values(color).all(10)
+      setError(false)
+      setList(colors)
+    } catch (error) {
+      setError(true)
+    }
   }
   return (
     <>
@@ -18,10 +25,26 @@ function App() {
             type='text'
             value={color}
             onChange={(e) => setColor(e.target.value)}
+            placeholder='#f15025'
+            className={`${error ? 'error' : null}`}
           />
+          <button className='btn' type='submit'>
+            submit
+          </button>
         </form>
       </section>
-      <section className='color'></section>
+      <section className='colors'>
+        {list.map((color, index, hexColor) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          )
+        })}
+      </section>
     </>
   )
 }
